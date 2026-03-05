a1fd028bcb279e7c64c1ee41825307fccc5226bc
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-05 18:08:48 +0700

    Prosedur input2

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -23,3 +23,12 @@ void input()
     }
 }
 cout << endl;
+cout << "====================" << endl;
+cout << "Masukkan Elemen Array" << endl;
+cout << "====================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
