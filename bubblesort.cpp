1c735a9977ff40c2fc5a4fcb75a8ec703c9c5da4
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-05 17:59:19 +0700

    Prosedur input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -7,3 +7,19 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{ //prosedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
+cout << endl;
