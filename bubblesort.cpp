bbf9d508253749aabd0ffcb6ab80808b86f21020
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-05 18:52:17 +0700

    Final 2

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -21,16 +21,16 @@ void input()
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
-}
-cout << endl;
-cout << "====================" << endl;
-cout << "Masukkan Elemen Array" << endl;
-cout << "====================" << endl;
-
-for (int i = 0; i < n; i++)
-{
+    cout <<endl;
+    cout << "====================" <<endl;
+    cout << "Masukkan Elemen Array" <<endl;
+    cout << "====================" <<endl;
+    
+    for (int i = 0; i < n; i++)
+    {
     cout << "Data ke-" << (i + 1) << ": ";
     cin >> arr[i];
+    }
 }
 void bubbleSortArray()
 {
